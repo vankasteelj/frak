98050a52b5efa1091b017128d781192fb7cf0bae
vankasteelj <vankasteelj@gmail.com>
2018-12-31 17:11:50 +0100

    network: assing port on client connect

--- app/js/lib/network.js
+++ app/js/lib/network.js
@@ -1,30 +1,24 @@
 'use strict'
 
 const Network = {
-    ports: {
-        main: 3000,
-        play: 3001
-    },
-    servers: {
-        main: null,
-        play: null
-    },
-    connectedServers: [],
-    addServers: (servers = []) => {
+    port: 3250,
+    server: null,
+    peers: [],
+    addPeers: (servers = []) => {
         for (let toAdd in servers) {
             let exists;
-            for (let existing in Network.connectedServers) {
-                if (Network.connectedServers[existing].ip === servers[toAdd].ip) exists = true;
+            for (let existing in Network.peers) {
+                if (Network.peers[existing].ip === servers[toAdd].ip) exists = true;
             }
             if (!exists && (DB.get('localip') !== servers[toAdd].ip)) {
-                console.log('Network: local server found (%s @ %s)', servers[toAdd].ip, servers[toAdd].name);
-                Network.connectedServers.push(servers[toAdd]);
-                Network.checkServer(servers[toAdd]);
+                console.log('Network: peer connected (%s @ %s)', servers[toAdd].ip, servers[toAdd].name);
+                Network.peers.push(servers[toAdd]);
+                Network.checkPeer(servers[toAdd]);
             }
         }
     },
-    checkServer: (server) => {
-        got(`http://${server.ip}:${Network.ports.main}`, {
+    checkPeer: (server) => {
+        got(`http://${server.ip}:${Network.port}`, {
             headers: {
                 'client': JSON.stringify({
                     ip: DB.get('localip'),
@@ -32,25 +26,50 @@ const Network = {
                 })
             }
         }).then(res => {
-            let body = JSON.parse(res.body);
-
-            for (let existing in Network.connectedServers) {
-                if (Network.connectedServers[existing].ip === server.ip) {
-                    Network.connectedServers[existing].name = body.name;
-                    Network.connectedServers[existing].movies = body.movies;
-                    Network.connectedServers[existing].shows = body.shows;
+            for (let existing in Network.peers) {
+                if (Network.peers[existing].ip === server.ip) {
+                    Network.peers[existing] = JSON.parse(res.body);
+                    Network.getFreePort().then(port => {
+                        Network.peers[existing].port = port;
+                    });
                 }
             }
 
-            setTimeout(() => Network.checkServer(server), 30000);
+            setTimeout(() => Network.checkPeer(server), 30000);
         }).catch(() => {
-            for (let existing in Network.connectedServers) {
-                if (Network.connectedServers[existing].ip === server.ip) {
-                    Network.connectedServers.splice(existing, 1);
-                    console.log('Network: %s @ %s disconnected', server.ip, server.name);
+            for (let existing in Network.peers) {
+                if (Network.peers[existing].ip === server.ip) {
+                    Network.peers.splice(existing, 1);
+                    console.log('Network: peer disconnected (%s @ %s)', server.ip, server.name);
                 }
             }
         });
+    },
+    findPeers: () => {
+        let localip = DB.get('localip');
+        let baseIp = localip.match(/\d+\.\d+\.\d+\./)[0];
+        let ips = [];
+
+        for (let i = 1; i < 255; i++) ips.push(baseIp + i);
+
+        Promise.all(ips.map(ip => {
+            return new Promise((resolve, reject) => {
+                got('http://' + ip + ':' + Network.port, {
+                    timeout: 500,
+                    headers: {
+                        'client': JSON.stringify({
+                            ip: DB.get('localip'),
+                            name: process.env.COMPUTERNAME
+                        })
+                    }
+                }).then(res => {
+                    resolve(JSON.parse(res.body));
+                }).catch(() => resolve());
+            });
+        })).then((responses) => {
+            responses = responses.filter(n => n); // remove empty from array
+            Network.addPeers(responses);
+        }).catch(console.error);
     },
     buildMainServer: () => {
         //build json
@@ -65,25 +84,26 @@ const Network = {
         };
 
         // only one main server running at a time
-        if (Network.servers.main) {
-            Network.servers.main.close();
-            Network.servers.main = null;
+        if (Network.server) {
+            Network.server.close();
+            Network.server = null;
         }
 
         //serve json
-        Network.servers.main = http.createServer((req, res) => {
+        Network.server = http.createServer((req, res) => {
             let client = JSON.parse(req.headers.client);
 
             // on GET, register the client and send back the json api
             if (req.method === 'GET') {
-                //req.headers.client is the client IP
-                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.writeHead(200, {
+                    'Content-Type': 'application/json'
+                });
                 res.write(JSON.stringify(json));
                 res.end();
 
-                Network.addServers([client]);
+                Network.addPeers([client]);
 
-            // on POST, serve the file to a new server and send back the url
+                // on POST, serve the file to a new server and send back the url
             } else if (req.method === 'POST') {
                 let body = '';
                 req.on('data', (data) => {
@@ -92,62 +112,63 @@ const Network = {
                 req.on('end', () => {
                     let file = JSON.parse(body);
 
-                    // only one play server running at a time (TODO: allow 1 per client)
-                    if (Network.servers.play) {
-                        Network.servers.play.close();
-                        Network.servers.play = null;
+                    // serve the file on assigned port
+                    for (let existing in Network.peers) {
+                        if (Network.peers[existing].ip === client.ip) {
+
+                            // only one play server running at a time
+                            if (Network.peers[existing].playing) {
+                                Network.peers[existing].playing.close();
+                                Network.peers[existing].playing = null;
+                            }
+
+                            Network.peers[existing].playing = http.createServer((req2, res2) => {
+                                res2.writeHead(200, {
+                                    'Content-Type': 'video/mp4',
+                                    'Content-Length': file.size
+                                });
+                                let readStream = fs.createReadStream(file.path);
+                                readStream.pipe(res2);
+                            });
+                            Network.peers[existing].playing.listen(Network.peers[existing].port);
+
+                            console.log('Network: serving \'%s\' on port %d (requested by %s @ %s)', file.filename, Network.peers[existing].port, client.ip, client.name);
+                            res.writeHead(200, {
+                                'Content-Type': 'application/json'
+                            });
+                            res.write(JSON.stringify({
+                                file: file,
+                                url: `http://${json.ip}:${port}`
+                            }));
+                            res.end();
+                        }
                     }
-
-                    // serve the file
-                    Network.servers.play = http.createServer((req2, res2) => {
-                        res2.writeHead(200, {
-                            'Content-Type': 'video/mp4',
-                            'Content-Length': file.size
-                        });
-                        let readStream = fs.createReadStream(file.path);
-                        readStream.pipe(res2);
-                    }).listen(Network.ports.play);
-
-                    console.log('Serving \'%s\' on port %d (requested by %s @ %s)', file.filename, Network.ports.play, client.ip, client.name);
-
-                    res.writeHead(200, {'Content-Type': 'application/json'});
-                    res.write(JSON.stringify({
-                        file: file,
-                        url: `http://${json.ip}:${Network.port.play}`
-                    }));
-                    res.end();
                 });
             }
         });
 
-        Network.servers.main.listen(Network.ports.main);
-        console.log('Local server running on http://%s:%d', json.ip, Network.ports.main);
+        Network.server.listen(Network.port);
+        console.log('Network: local server running on http://%s:%d', json.ip, Network.port);
         Network.findPeers();
     },
-    findPeers: () => {
-        let localip = DB.get('localip');
-        let baseIp = localip.match(/\d+\.\d+\.\d+\./)[0];
-        let ips = [];
+    getFreePort: (port = Network.port+1) => {
+        return new Promise((resolve, reject) => {
+            for (let existing in Network.peers) {
+                if (Network.peers[existing].port === port) return resolve(Network.getFreePort(port+=1));
+            }
 
-        for (let i = 1; i < 255; i++) ips.push(baseIp+i);
+            let server = http.createServer();
 
-        Promise.all(ips.map(ip => {
-          return new Promise((resolve, reject) => {
-            got('http://'+ip+':3000', {
-                timeout: 500,
-                headers: {
-                    'client': JSON.stringify({
-                        ip: DB.get('localip'),
-                        name: process.env.COMPUTERNAME
-                    })
-                }
-            }).then(res => {
-                resolve(JSON.parse(res.body));
-            }).catch(() => resolve());
-          });
-        })).then((responses) => {
-            responses = responses.filter(n => n); // remove empty from array
-            Network.addServers(responses);
-        }).catch(console.error);
+            server.once('error', (err) => {
+                return resolve(Network.getFreePort(port+=1));
+            });
+
+            server.once('listening', () => {
+                server.close();
+                return resolve(port);
+            });
+
+            server.listen(port);
+        });
     }
 };
