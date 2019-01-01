eb289e201856eab514feeb020352c790470c5fac
vankasteelj <vankasteelj@gmail.com>
2019-01-01 19:39:04 +0100

    network: allow seeking on stream

--- app/js/lib/network.js
+++ app/js/lib/network.js
@@ -32,7 +32,7 @@ const Network = {
                     Network.peers[existing].movies = body.movies;
                     Network.peers[existing].shows = body.shows;
                     Network.peers[existing].name = body.name;
-                    
+
                     Network.getFreePort(server.ip).then(port => {
                         Network.peers[existing].port = port;
                     });
@@ -119,7 +119,6 @@ const Network = {
                     // serve the file on assigned port
                     for (let existing in Network.peers) {
                         if (Network.peers[existing].ip === client.ip) {
-                            console.log('BUILD PLAY SERVER')
                             Network.buildPlayServer(file, existing);
 
                             res.writeHead(200, {
@@ -147,32 +146,47 @@ const Network = {
             Network.peers[clientId].playing = null;
         }
 
-        let fileStream = fs.createReadStream(file.path);
-
         Network.peers[clientId].playing = http.createServer((req, res) => {
-            console.log('Network: serving \'%s\' to %s @ %s', file.filename, Network.peers[clientId].ip, Network.peers[clientId].name);
-            res.writeHead(200, {
-                'Content-Type': 'video/mp4',
-                'Content-Length': file.size
-            });
-            fileStream.pipe(res);
+            let range = req.headers.range;
+
+            if (range) {
+                let parts = range.replace(/bytes=/, "").split("-");
+                let start = parseInt(parts[0], 10);
+                let end = parts[1] ? parseInt(parts[1], 10) : file.size - 1;
+                let chunksize = (end - start) + 1;
+                let fileStream = fs.createReadStream(file.path, {start, end});
+
+                res.writeHead(206, {
+                    'Content-Range': `bytes ${start}-${end}/${file.size}`,
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': chunksize,
+                    'Content-Type': 'video/mp4',
+                });
+                fileStream.pipe(res);
+            } else {
+                res.writeHead(200, {
+                    'Content-Length': file.size,
+                    'Content-Type': 'video/mp4',
+                });
+                fs.createReadStream(file.path).pipe(res);
+            }
         });
 
         Network.peers[clientId].playing.listen(Network.peers[clientId].port);
 
-        console.log('Network: \'%s\' ready on port %d (requested by %s @ %s)', file.filename, Network.peers[clientId].port, Network.peers[clientId].ip, Network.peers[clientId].name);
+        console.log('Network: \'%s\' ready to stream on port %d (requested by %s @ %s)', file.filename, Network.peers[clientId].port, Network.peers[clientId].ip, Network.peers[clientId].name);
     },
-    getFreePort: (ip, port = Network.port+1) => {
+    getFreePort: (ip, port = Network.port + 1) => {
         return new Promise((resolve, reject) => {
             for (let existing in Network.peers) {
                 if (Network.peers[existing].port === port && Network.peers[existing].ip === ip) return resolve(port);
-                if (Network.peers[existing].port === port && Network.peers[existing].ip !== ip) return resolve(Network.getFreePort(port+=1));
+                if (Network.peers[existing].port === port && Network.peers[existing].ip !== ip) return resolve(Network.getFreePort(port += 1));
             }
 
             let server = http.createServer();
 
             server.once('error', (err) => {
-                return resolve(Network.getFreePort(port+=1));
+                return resolve(Network.getFreePort(port += 1));
             });
 
             server.once('listening', () => {
