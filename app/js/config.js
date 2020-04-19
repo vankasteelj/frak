9e6d99e1482a6c2c947ee71ed939035615ac279d
Jean van Kasteel <vankasteelj@gmail.com>
2020-04-19 14:06:12 +0200

    update tracker default list

--- app/js/config.js
+++ app/js/config.js
@@ -51,16 +51,18 @@ const Settings = {
         webSeeds: true,
         announce: [
             'udp://tracker.coppersurfer.tk:6969/announce',
-            'udp://glotorrents.pw:6969/announce', 
             'udp://exodus.desync.com:6969/announce',
             'udp://tracker.opentrackr.org:1337/announce',
             'udp://9.rarbg.com:2710/announce',
-            'udp://tracker.openbittorrent.com:80',
             'udp://tracker.publicbt.com:80/announce',
             'udp://tracker.empire-js.us:1337',
+            'udp://tracker.leechers-paradise.org:6969/announce',
+            'udp://tracker.pirateparty.gr:6969/announce',
+            'udp://tracker.internetwarriors.net:1337/announce',
             'wss://tracker.openwebtorrent.com',
             'wss://tracker.fastcast.nz',
-            'wss://tracker.btorrent.xyz'
+            'wss://tracker.btorrent.xyz',
+            'http://torrent.nwps.ws/announce'
         ]
     },
     supportedVideoFiles: ['.mkv','.avi','.mp4','.m4v','.mts','.m2ts', '.ts']
