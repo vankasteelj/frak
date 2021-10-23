30e18d3115db169d2f593e3b05579db18f3cc12d
vankasteelj <vankasteelj@gmail.com>
2021-10-23 19:59:47 +0200

    updates

--- app/js/lib/images.js
+++ app/js/lib/images.js
@@ -42,8 +42,9 @@ const Images = {
   reduce: (link, full) => {
     if (!link) return null
 
-    full && link.match('assets.fanart.tv') && (link = link.replace('fanart.tv/fanart', 'fanart.tv/preview'))
-    link.match('image.tmdb.org') && (link = link.replace('w780', 'w342').replace('/original/', '/w1280/'))
+    full && link.match('fanart.tv') && (link = link.replace('fanart.tv/fanart', 'fanart.tv/preview'))
+    link.match('tmdb.org') && (link = link.replace('w780', 'w342').replace('/original/', '/w1280/'))
+    link.match('thetvdb.com') && (link = link.replace('.jpg', '_t.jpg'))
 
     return link
   }
