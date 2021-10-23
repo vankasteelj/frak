e62b33604515e553e6615350a00011cb353da52c
vankasteelj <vankasteelj@gmail.com>
2021-10-23 14:13:49 +0200

    try sleep 1500 for load next
    - about #125 watch next episode is sometimes broken

--- app/js/lib/trakt.js
+++ app/js/lib/trakt.js
@@ -319,7 +319,7 @@ const Trakt = {
           // display spinner on list
           Player.config.model.show && $(`#collection #${Player.config.model.show.ids.slug}`).append('<div class="item-spinner"><div class="fa fa-spin fa-refresh"></div>')
 
-          Misc.sleep(800).then(() => {
+          Misc.sleep(1500).then(() => {
             return Trakt.reload(true, type, Details.model.show.ids.slug)
           }).then(collections => {
             Details.loadNext()
