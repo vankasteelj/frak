vankasteelj/frak

commit 7f9b7e2900659f4659b5ac3e8c3a14651fc3cc64

standard:fix

app/js/lib/network.js
@@ -173,7 +173,7 @@ const Network = {
 
     // serve the file on assigned port
     Network.peers[clientId].playbackServer = http.createServer((req, res) => {
-      const pathname = require('url').parse(req.url).pathname
+      const pathname = new (require('url')).URL(req.url).pathname
       switch (pathname) {
         case '/subtitles': {
           // only working for a ".srt file" using the exact same name as the video file

app/js/app.js
@@ -9,7 +9,7 @@ try {
     console.debug('Running in development')
     win.showDevTools()
   }
-  
+
   // Set up everything
   Boot.preload()
   Trakt.reconnect()
@@ -67,7 +67,7 @@ const onOpenApp = () => {
 onOpenApp()
 
 win.on('close', () => {
-  try {Subtitles.client.logout()} catch(e) {}
+  try { Subtitles.client.logout() } catch (e) { }
   Cache.delete()
   win.tray.remove()
   nw.global.playerPopup && nw.global.playerPopup.close(true)

app/js/config.js
@@ -26,12 +26,12 @@ const Settings = {
     multimonitor: false,
     monitor: 1
   },
-  apikeys: JSON.parse(atob('eyJ0cmFrdF9pZCI6ImY3YjY1MjhhYzhiY2IzNjc0MjJhZWU1M' + 
-    'WNlYjkwZDg2ZDdlMzcyYzMyNzljMDQ1NGIyYTk2ZTEzZGQzYTc1NDIiLCJ0cmFrdF9zZWNyZ' + 
-    'XQiOiI5MTY3NWM2M2NjNzljOTRkYzE1NzliNTExMjVjMmQwYjA5NTQ5MDFjMmFjZTA0ODNlZ' + 
-    'DQzN2Q1NDBjMjg2OWZhIiwiZmFuYXJ0IjoiMjVkNjAwNzVjYjVmNTk4Mjg0ZjU1OGRjZmYzN' + 
-    'ThkNzQiLCJ0bWRiIjoiMjcwNzUyODJlMzllZWE3NmJkOTYyNmVlNWQzZTc2N2IiLCJvcGVuc' + 
-    '3VidGl0bGVzIjoibXFmSEJKeWtPU0w1VVBsSDA4MWtKeTJTdWlxcUNrU1QiLCJvbWRiIjoiY' + 
+  apikeys: JSON.parse(atob('eyJ0cmFrdF9pZCI6ImY3YjY1MjhhYzhiY2IzNjc0MjJhZWU1M' +
+    'WNlYjkwZDg2ZDdlMzcyYzMyNzljMDQ1NGIyYTk2ZTEzZGQzYTc1NDIiLCJ0cmFrdF9zZWNyZ' +
+    'XQiOiI5MTY3NWM2M2NjNzljOTRkYzE1NzliNTExMjVjMmQwYjA5NTQ5MDFjMmFjZTA0ODNlZ' +
+    'DQzN2Q1NDBjMjg2OWZhIiwiZmFuYXJ0IjoiMjVkNjAwNzVjYjVmNTk4Mjg0ZjU1OGRjZmYzN' +
+    'ThkNzQiLCJ0bWRiIjoiMjcwNzUyODJlMzllZWE3NmJkOTYyNmVlNWQzZTc2N2IiLCJvcGVuc' +
+    '3VidGl0bGVzIjoibXFmSEJKeWtPU0w1VVBsSDA4MWtKeTJTdWlxcUNrU1QiLCJvbWRiIjoiY' +
     'jI3Y2FlN2MiLCJ0dmRiIjoiYTlkYWQ0ODItMjhmYi00ZTRjLTg1YTgtNGY2MjExZmM2YmMzIn0='
   )),
   streamer: {

app/js/lib/cast.js
@@ -49,8 +49,8 @@ const Cast = {
         if (subtitle) {
           const port = 8000
           const subtitlesUrl = 'http://' + DB.sync.get('localip') + ':' + port + '/dlnacaptions.srt'
-          
-          //build srt server
+
+          // build srt server
           Cast.subtitleServer = http.createServer((req, res) => {
             res.writeHead(200, {
               'Content-type': 'text/srt',
@@ -62,7 +62,7 @@ const Cast = {
             fs.createReadStream(subtitle).pipe(res)
           })
 
-          //serve srt
+          // serve srt
           Cast.subtitleServer.listen(port)
           media.subtitle = [subtitlesUrl]
         }

app/js/main/collection.js
@@ -7,7 +7,7 @@ const Collection = {
   load: () => {
     console.info('Loading collection')
 
-    scheduler.postTask(Collection.hiddenMovies.verify, {priority: 'background'})
+    scheduler.postTask(Collection.hiddenMovies.verify, { priority: 'background' })
 
     // should trakt update?
     Promise.all([
@@ -35,7 +35,7 @@ const Collection = {
       }
     }).catch(console.error)
 
-    scheduler.postTask(Collection.get.local, {priority: 'background'})
+    scheduler.postTask(Collection.get.local, { priority: 'background' })
   },
 
   get: {

app/js/main/details.js
@@ -695,14 +695,13 @@ const Details = {
 
     const title = $('#details-metadata .titles').text().replace(/\W+/g, ' ')
     const url = Streamer.streaminfo.url.replace('127.0.0.1', DB.sync.get('localip'))
-    let subtitle = undefined 
 
+    let subtitle
     Player.mpv.getProperty('current-tracks/sub').then(sub => {
-      if (sub && sub.external) {
-        subtitle = sub['external-filename']
-      }
-      return
-    }).catch(err => {}).finally(() => {
+      if (sub && sub.external) subtitle = sub['external-filename']
+    }).catch(err => {
+      console.error('Details.keepWatchingDlna - Could not get subtitle from MPV', err)
+    }).finally(() => {
       Cast.cast(player.name, title, url, subtitle)
     })
   },

app/js/utils/boot.js
@@ -10,39 +10,39 @@ const Boot = {
     Boot.online() // check if online
     Boot.startScreen() // what view to load first
     Misc.events = new (require('node:events'))() // set up events
-    scheduler.postTask(Boot.tray, {priority: 'background'}) // setup the tray 
-    scheduler.postTask(Cache.create, {priority: 'background'}) // create tmp dir
-    scheduler.postTask(IB.create, {priority: 'background'}) // create ImagesBank folder
-    scheduler.postTask(Boot.setupSettings, {priority: 'background'}) // setup settings popup
-    scheduler.postTask(Interface.buildSwitch, {priority: 'background'}) // switch trakt account screen
+    scheduler.postTask(Boot.tray, { priority: 'background' }) // setup the tray
+    scheduler.postTask(Cache.create, { priority: 'background' }) // create tmp dir
+    scheduler.postTask(IB.create, { priority: 'background' }) // create ImagesBank folder
+    scheduler.postTask(Boot.setupSettings, { priority: 'background' }) // setup settings popup
+    scheduler.postTask(Interface.buildSwitch, { priority: 'background' }) // switch trakt account screen
     document.addEventListener('contextmenu', (e) => e.preventDefault()) // remove default right clicks
   },
   postload: () => {
-    scheduler.postTask(Player.findMpv, {priority: 'background'}) // player
-    scheduler.postTask(Plugins.load, {priority: 'background'}) // load search plugins
-    scheduler.postTask(Boot.setupInputs, {priority: 'background'}) // browse button
-    scheduler.postTask(Keyboard.setupShortcuts, {priority: 'background'}) // keyboard shortcuts
+    scheduler.postTask(Player.findMpv, { priority: 'background' }) // player
+    scheduler.postTask(Plugins.load, { priority: 'background' }) // load search plugins
+    scheduler.postTask(Boot.setupInputs, { priority: 'background' }) // browse button
+    scheduler.postTask(Keyboard.setupShortcuts, { priority: 'background' }) // keyboard shortcuts
     // Gamepad.init(); // gamepad support - needs work
-    scheduler.postTask(Boot.setupScreens, {priority: 'background'}) // nwjs screen listener
-    scheduler.postTask(Update.check, {priority: 'background'}) // update
-    scheduler.postTask(Dragdrop.setup, {priority: 'background'}) // allow drag&drop
-    scheduler.postTask(Subtitles.opensubReLogin, {priority: 'background'}) // opensubtitles login if needed
-    scheduler.postTask(Ratings.setupDropdown, {priority: 'background'}) // ratings init
-    scheduler.postTask(Boot.setupVersion, {priority: 'background'}) // version number
+    scheduler.postTask(Boot.setupScreens, { priority: 'background' }) // nwjs screen listener
+    scheduler.postTask(Update.check, { priority: 'background' }) // update
+    scheduler.postTask(Dragdrop.setup, { priority: 'background' }) // allow drag&drop
+    scheduler.postTask(Subtitles.opensubReLogin, { priority: 'background' }) // opensubtitles login if needed
+    scheduler.postTask(Ratings.setupDropdown, { priority: 'background' }) // ratings init
+    scheduler.postTask(Boot.setupVersion, { priority: 'background' }) // version number
 
     // periodical tasks
-    scheduler.postTask(Boot.cleanup, {priority: 'background'}) // periodically cleanup
-    scheduler.postTask(Boot.idle, {priority: 'background'}) // periodically update
+    scheduler.postTask(Boot.cleanup, { priority: 'background' }) // periodically cleanup
+    scheduler.postTask(Boot.idle, { priority: 'background' }) // periodically update
 
     // right clicks
-    scheduler.postTask(Interface.rightClickNav, {priority: 'background'})
-    scheduler.postTask(() => Boot.setupRightClicks('input[type=text], textarea'), {priority: 'background'})
+    scheduler.postTask(Interface.rightClickNav, { priority: 'background' })
+    scheduler.postTask(() => Boot.setupRightClicks('input[type=text], textarea'), { priority: 'background' })
   },
 
   // STARTUP: setup network connexion
   online: () => {
     const ifaces = os.networkInterfaces()
-    const values = [].concat.apply([], Object.keys(ifaces).map(n => ifaces[n])).filter(v => v.family == 'IPv4' && v.internal == false)
+    const values = [].concat.apply([], Object.keys(ifaces).map(n => ifaces[n])).filter(v => v.family === 'IPv4' && v.internal === false)
     const localip = values.length ? values[0].address : '127.0.0.1'
 
     DB.sync.store(localip, 'localip')
@@ -254,7 +254,7 @@ const Boot = {
     // allow DLNA search
     if (DB.sync.get('dlnacasting')) {
       document.querySelector('#allow_dlnacasting').checked = true
-      scheduler.postTask(Cast.scan, {priority: 'background'})
+      scheduler.postTask(Cast.scan, { priority: 'background' })
     }
 
     // allow direct playback feature on local network
@@ -321,12 +321,12 @@ const Boot = {
     // size of image cache
     scheduler.postTask(() => {
       IB.calcSize().then(s => $('#imagecachesize').text(s)).catch(console.log)
-    }, {priority: 'background'})
+    }, { priority: 'background' })
 
     // size of video cache
     scheduler.postTask(() => {
       Cache.calcSize().then(s => $('#videocachesize').text(s)).catch(console.log)
-    }, {priority: 'background'})
+    }, { priority: 'background' })
   },
 
   setupInputs: () => {

app/js/utils/localization.js
@@ -78,7 +78,7 @@ const Localization = {
           c[j].innerText = i18n.__(c[j].innerText)
         }
       }
-    }, {priority: 'background'})
+    }, { priority: 'background' })
   },
 
   // STARTUP: build dropdown menu for changing app localization

commit a6262c5f55d10edeca6d39e4a6cfc86822b93d11

fix local playback not working
- bug introduced in commit 7f9b7e2900659f4659b5ac3e8c3a14651fc3cc64
- url.parse().pathname cannot be replaced by new URL().pathname if the
  url is '/' or '/subtitles' like here.

app/js/lib/network.js
@@ -173,8 +173,7 @@ const Network = {
 
     // serve the file on assigned port
     Network.peers[clientId].playbackServer = http.createServer((req, res) => {
-      const pathname = new (require('url')).URL(req.url).pathname
-      switch (pathname) {
+      switch (req.url) {
         case '/subtitles': {
           // only working for a ".srt file" using the exact same name as the video file
           const ext = file.filename.split('.').pop()